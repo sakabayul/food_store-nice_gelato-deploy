ab72258723f7970036f7183b25d44547138f2f7c
sakabayul <sakapkl015@gmail.com>
2025-02-28 17:09:20 +0700

    [DEPLOY]

---
+++ assets/list-data/products.ts
@@ -0,0 +1,52 @@
+export interface product{
+    id: number;
+    discount: number;
+    img: string;
+    name: string;
+    price: number;
+}
+
+export const products = [
+    {
+        id: 1,
+        discount: 10,
+        img: '../../assets/image/product-assets/img-1.jpg',
+        name: 'Gelato Chocolate',
+        price: 15
+    },
+    {
+        id: 2,
+        discount: 20,
+        img: '../../assets/image/product-assets/img-2.jpg',
+        name: 'Gelato Vanilla x Strawbarry',
+        price: 20
+    },
+    {
+        id: 3,
+        discount: 15,
+        img: '../../assets/image/product-assets/img-3.jpg',
+        name: 'Gelato Matcha',
+        price: 10
+    },
+    {
+        id: 4,
+        discount: 15,
+        img: '../../assets/image/product-assets/img-4.jpg',
+        name: 'Gelato Vanilla',
+        price: 12
+    },
+    {
+        id: 5,
+        discount: 15,
+        img: '../../assets/image/product-assets/img-5.jpg',
+        name: 'Gelato Orange',
+        price: 17
+    },
+    {
+        id: 6,
+        discount: 15,
+        img: '../../assets/image/product-assets/img-6.jpg',
+        name: 'Gelato Matcha',
+        price: 19
+    }
+];
---
+++ assets/list-data/reviews.ts
@@ -0,0 +1,51 @@
+export interface review {
+    id:number;
+    star:number;
+    description:string;
+    img:string;
+    name:string;
+    status:string;
+}
+
+export const reviews = [
+    {
+        id: 1,
+        star: 4,
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti asperiores laboriosam praesentium
+        enim maiores? Ad repellat voluptates alias facere repudiandae dolor accusamus enim ut odit, aliquam
+        nesciunt eaque nulla dignissimos.`,
+        img: '../assets/image/pic-assets/pic-1.jpg',
+        name: 'Kadita Puspita',
+        status: 'Happy Customer'
+    },
+    {
+        id: 2,
+        star: 5,
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti asperiores laboriosam praesentium
+        enim maiores? Ad repellat voluptates alias facere repudiandae dolor accusamus enim ut odit, aliquam
+        nesciunt eaque nulla dignissimos.`,
+        img: '../assets/image/pic-assets/pic-2.jpg',
+        name: 'Lestari Nestle',
+        status: 'Happy Customer'
+    },
+    {
+        id: 3,
+        star: 3,
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti asperiores laboriosam praesentium
+        enim maiores? Ad repellat voluptates alias facere repudiandae dolor accusamus enim ut odit, aliquam
+        nesciunt eaque nulla dignissimos.`,
+        img: '../assets/image/pic-assets/pic-3.jpg',
+        name: 'Raditya',
+        status: 'Happy Customer'
+    },
+    {
+        id: 4,
+        star: 4,
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti asperiores laboriosam praesentium
+        enim maiores? Ad repellat voluptates alias facere repudiandae dolor accusamus enim ut odit, aliquam
+        nesciunt eaque nulla dignissimos.`,
+        img: '../assets/image/pic-assets/pic-4.jpg',
+        name: 'Tia Wulandari',
+        status: 'Happy Customer'
+    }
+]
